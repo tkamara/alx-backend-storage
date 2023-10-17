05b60d86666bdce9a4eacd047d7c528f8465d832
Chebet Kamara <chebetkamara@gmail.com>
2023-10-17 03:18:44 -0700

    Fixing errors

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -8,4 +8,4 @@ def schools_by_topic(mongo_collection, topic):
     returns the list of school having a specific topic
     """
     res = mongo_collection.find({ "topic": topic })
-    return list(res)
+    return res
